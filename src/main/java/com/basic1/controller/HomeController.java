0345f66c3389cbd2cb812fc54cc573e6a949618d
SangWon7242 <gotkdqja@gmailc.om>
2024-06-25 21:35:36 +0900

    C:/Program Files/Git/home/increase에 대한 요청 처리

--- src/main/java/com/basic1/controller/HomeController.java
+++ src/main/java/com/basic1/controller/HomeController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 // HomeController는 컨트롤러이다.
 @Controller
 public class HomeController {
+  private int count;
+
+  public HomeController() {
+    count = -1;
+  }
+
   // @GetMapping("/home/main") 의 의미
   // 개발자가 스프링부트한테 말한다.
   // 만약에 /home/main 요청이 들어오면 아래 메서드를 실행시켜줘
@@ -32,5 +38,12 @@ public class HomeController {
   @ResponseBody
   public String showHome3() {
     return "스프링부트는 획기적입니다.";
+  }
+
+  @GetMapping("/home/increase")
+  @ResponseBody
+  public int showIncrease() {
+    count++;
+    return count;
   }
 }
