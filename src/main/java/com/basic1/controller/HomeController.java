2de29253dcbcd5159065d07393738747c2444175
SangWon7242 <gotkdqja@gmailc.om>
2024-06-27 20:45:46 +0900

    사람 추가

--- src/main/java/com/basic1/controller/HomeController.java
+++ src/main/java/com/basic1/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.basic1.controller;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,9 +21,11 @@ import java.util.Map;
 @Controller
 public class HomeController {
   private int count;
+  private List<Person> people;
 
   public HomeController() {
     count = -1;
+    people = new ArrayList<>();
   }
 
   // @GetMapping("/home/main") 의 의미
@@ -200,6 +203,24 @@ public class HomeController {
 
     return list;
   }
+
+  @GetMapping("/home/addPerson")
+  @ResponseBody
+  public String addPerson(String name, int age) {
+    Person p = new Person(name, age);
+
+    System.out.println(p);
+
+    people.add(p);
+
+    return "%d번 사람이 추가되었습니다.".formatted(p.getId());
+  }
+
+  @GetMapping("/home/showPeople")
+  @ResponseBody
+  public List<Person> showPeople() {
+    return people;
+  }
 }
 
 class Car {
@@ -241,4 +262,22 @@ class Car2 {
   private String name;
   @Setter
   private List<Integer> carNo;
+}
+
+@AllArgsConstructor
+@Getter
+@ToString
+class Person {
+  private static int lastId;
+  private final int id;
+  private final String name;
+  private final int age;
+
+  static {
+    lastId = 0;
+  }
+
+  public Person(String name, int age) {
+    this(++lastId, name, age);
+  }
 }
