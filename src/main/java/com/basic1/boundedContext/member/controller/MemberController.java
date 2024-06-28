91230c76254ccf0d636e5a08ac51b311d770b288
SangWon7242 <gotkdqja@gmailc.om>
2024-06-28 20:41:04 +0900

    member, memberRepository 도입

--- src/main/java/com/basic1/boundedContext/member/controller/MemberController.java
+++ src/main/java/com/basic1/boundedContext/member/controller/MemberController.java
@@ -20,13 +20,13 @@ public class MemberController {
   @GetMapping("/member/login")
   @ResponseBody
   public RsData login(String username, String password) {
+    if(username.trim().isEmpty()) {
+      return RsData.of("F-3", "username(을)를 입력해주세요.");
+    }
 
-    /*
-    Map<String, Object> rsData = new LinkedHashMap<>() {{
-      put("rsultCode", "S-1");
-      put("msg", "%s님 환영합니다.".formatted(username));
-    }};
-    */
+    if(password.trim().isEmpty()) {
+      return RsData.of("F-4", "password(을)를 입력해주세요.");
+    }
 
     return memberService.tryLogin(username, password);
   }
---
+++ src/main/java/com/basic1/boundedContext/member/repository/MemberRepository.java
@@ -0,0 +1,33 @@
+package com.basic1.boundedContext.member.repository;
+
+import com.basic1.boundedContext.member.entity.Member;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberRepository {
+  private List<Member> members;
+
+  public MemberRepository() {
+    members = new ArrayList<>();
+
+    members.add(new Member("user1", "1234"));
+    members.add(new Member("abc", "12345"));
+    members.add(new Member("test", "12346"));
+    members.add(new Member("love", "12347"));
+    members.add(new Member("like", "12348"));
+    members.add(new Member("giving", "12349"));
+    members.add(new Member("thanks", "123410"));
+    members.add(new Member("good", "123411"));
+    members.add(new Member("peace", "123412"));
+    members.add(new Member("hope", "123413"));
+  }
+
+
+  public Member findByUserName(String username) {
+    return members.stream()
+        .filter(member -> member.getUsername().equals(username))
+        .findFirst()
+        .orElse(null);
+  }
+}
